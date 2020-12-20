431366e97da44eb562ec8700efbfa4e75faf3b01
Nacho <josigmcs@gmail.com>
2020-12-20 14:24:31 +0100

    Cambios mass

--- pagina1.php
+++ pagina1.php
@@ -1,6 +1,7 @@
 <?php
 
-echo "<h1> TÍTULO </h1><br/>";
+echo "<h1> TÍTULO </h1><br/>"; 
+
 echo "<p> Texto dela página .... </p>";
 
 
---
+++ pagina2.php
@@ -0,0 +1,20 @@
+<?php
+
+echo "<h1> TÍTULO Página 2 </h1><br/><br/>";
+
+echo "<p> Texto de la página .... </p>";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
